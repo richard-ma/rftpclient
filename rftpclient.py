d3002685eea5853899b001a85c1bc5f6008caa5a
richard-ma <richard.ma.19850509@gmail.com>
2025-04-15 20:31:49 +0800

    优化命令提示，移除多余的输入提示信息

--- rftpclient.py
+++ rftpclient.py
@@ -44,8 +44,7 @@ if __name__ == "__main__":
 
     running = True
     while running:
-        print("rftpclient>>", end=" ")
-        command = input("Enter command (connect/disconnect): ").strip().lower()
+        command = input("rftpclient>>").strip().lower()
 
         if command in cmd_map:
             if command == "connect":
